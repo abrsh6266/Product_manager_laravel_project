5acc02cd11ff01676551b24b809d88d03e1df736
Abrham <abrsh6266@gmail.com>
2024-02-21 22:26:59 -0900

    creating category controller and view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+Route::get("/categories", [CategoryController::class, 'getCategories']);
 
 Route::get('/', function () {
     return view('welcome');
