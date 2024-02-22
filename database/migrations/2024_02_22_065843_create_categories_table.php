8a55e8155532a483d61a725842e98f50381535df
Abrham <abrsh6266@gmail.com>
2024-02-21 22:01:11 -0900

    creating categories table

--- database/migrations/2024_02_22_065843_create_categories_table.php
+++ database/migrations/2024_02_22_065843_create_categories_table.php
@@ -11,9 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('catergories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->boolean('is_active')->default(1);
             $table->timestamps();
+            
         });
     }
 
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('catergories');
+        Schema::dropIfExists('categories');
     }
 };
